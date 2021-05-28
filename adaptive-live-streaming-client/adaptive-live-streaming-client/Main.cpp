204f78d7527638877cc188f389aab35c28156441
kyungjin99 <kyungjin.kim.cs@gmail.com>
2021-05-28 21:35:27 +0900

    Refactor

--- adaptive-live-streaming-client/adaptive-live-streaming-client/Main.cpp
+++ adaptive-live-streaming-client/adaptive-live-streaming-client/Main.cpp
@@ -3,114 +3,124 @@
 #include <string>
 #include <vector>
 #include <fstream>
-#define MAXLINE 5000
 using namespace std;
 
-vector<string> vid_dvc_list; // video device list
-vector<string> aud_dvc_list; // audio device list
+constexpr int MAXLINE = 5000;
+vector<string> g_video_device_list; // video device list
+vector<string> g_audio_device_list; // audio device list
 
-string * trim(string * cmd_result) { // leave only sentences starting with "[dshow @"
-	int start = cmd_result->find("[dshow @");
-	if (start != string::npos) *cmd_result = cmd_result->substr(start, -1);
+string *Trim(string *device_command_output) // leave only sentences starting with "[dshow @"
+{ 
+    int start = device_command_output->find("[dshow @");
+    if (start != string::npos) *device_command_output = device_command_output->substr(start, -1);
 
-	int end = cmd_result->find("dummy");
-	if (end != string::npos) *cmd_result = cmd_result->substr(0, end);
-	return cmd_result;
+    int end = device_command_output->find("dummy");
+    if (end != string::npos) *device_command_output = device_command_output->substr(0, end);
+    return device_command_output;
 }
 
-void add_dvc_to_list(string line, int flag) {
-	int start = line.find("\"@device");
-	if (start != string::npos) {
-		line = line.substr(start, -1);
+void AddDeviceToList(string line, int flag) 
+{
+    int start = line.find("\"@device");
+    if (start != string::npos) {
+        line = line.substr(start, -1);
 
-		if (flag == 1) vid_dvc_list.push_back(line);
-		else if (flag == 2) aud_dvc_list.push_back(line);
-	}
+        if (flag == 1) g_video_device_list.push_back(line);
+        else if (flag == 2) g_audio_device_list.push_back(line);
+    }
 }
 
-void get_dvc_lists(string * cmd_result) {
-	int prev = 0;
-	int now = cmd_result->find('\n');
-	int flag = 0; // 1: video 2: audio
-	while (now != string::npos) {
-		string line = cmd_result->substr(prev, now - prev);
+void GetDeviceLists(string *device_command_output)
+{
+    int prev = 0;
+    int now = device_command_output->find('\n');
+    int flag = 0; // 1: video 2: audio
+    while (now != string::npos) {
+        string line = device_command_output->substr(prev, now - prev);
 
-		if (line.find("DirectShow video devices") != string::npos) flag = 1;
-		else if (line.find("DirectShow audio devices") != string::npos) flag = 2;
-		else add_dvc_to_list(line, flag);
+        if (line.find("DirectShow video devices") != string::npos) flag = 1;
+        else if (line.find("DirectShow audio devices") != string::npos) flag = 2;
+        else AddDeviceToList(line, flag);
 
-		prev = now + 1;
-		now = cmd_result->find('\n', prev);
-	}
-	string last_line = cmd_result->substr(prev, now - prev);
-	add_dvc_to_list(last_line, flag);
+        prev = now + 1;
+        now = device_command_output->find('\n', prev);
+    }
+    string last_line = device_command_output->substr(prev, now - prev);
+    AddDeviceToList(last_line, flag);
 }
 
-void read_from_cmd(FILE ** fp, string * cmd_result) {
-	string tmp;
-	char buf[MAXLINE];
+void ReadFromCommandLine(FILE **fp, string *device_command_output)
+{
+    string tmp;
+    char buf[MAXLINE];
 
-	while (fgets(buf, MAXLINE, *fp) != NULL) *cmd_result += buf; // fgets() == NULL indicates either EOF or I/O error
-	_pclose(*fp);
+    while (fgets(buf, MAXLINE, *fp) != NULL) *device_command_output += buf; // fgets() == NULL indicates either EOF or I/O error
+    _pclose(*fp);
 
-	trim(cmd_result); // "[dshow @"로 시작하는 문장만 남기고 다 삭제
-	get_dvc_lists(cmd_result);
+    Trim(device_command_output);
+    GetDeviceLists(device_command_output);
 }
 
-void write_to_bat(FILE ** bat) {
-	fprintf(*bat, "ffmpeg -f dshow -rtbufsize 100M -i ");
-	bool is_using_vid = vid_dvc_list.size() ? true : false;
-	bool is_using_aud = aud_dvc_list.size() ? true : false;
+void WriteBatchFile(FILE **bat) 
+{
+    fprintf(*bat, "ffmpeg -f dshow -rtbufsize 100M -i ");
+    bool is_using_video = g_video_device_list.size() ? true : false;
+    bool is_using_audio = g_audio_device_list.size() ? true : false;
 
-	if (is_using_vid) {
-		fprintf(*bat, "video=");
-		fprintf(*bat, vid_dvc_list[0].c_str());
-	}
+    if (is_using_video) 
+    {
+        fprintf(*bat, "video=");
+        fprintf(*bat, g_video_device_list[0].c_str());
+    }
 
-	if (is_using_aud) {
-		fprintf(*bat, ":audio=");
-		fprintf(*bat, aud_dvc_list[0].c_str());
-	}
+    if (is_using_audio) 
+    {
+        fprintf(*bat, ":audio=");
+        fprintf(*bat, g_audio_device_list[0].c_str());
+    }
 
-	fprintf(*bat, " ");
-	if (is_using_vid) fprintf(*bat, "-profile:v high -pix_fmt yuvj420p -level:v 4.1 -preset ultrafast -tune zerolatency -c:v h264 -r 10 -b:v 512k -s 640x360 ");
-	if (is_using_aud) fprintf(*bat, "-c:a aac -ac 2 -ab 32k -ar 44100 ");
-	fprintf(*bat, "-f flv -flush_packets 0 ");
-	fprintf(*bat, "rtmp://");
-	fprintf(*bat, "127.0.0.1"); // ip
-	fprintf(*bat, ":1935");
-	fclose(*bat); // close batch file
+    fprintf(*bat, " ");
+    if (is_using_video) fprintf(*bat, "-profile:v high -pix_fmt yuvj420p -level:v 4.1 -preset ultrafast -tune zerolatency -c:v h264 -r 10 -b:v 512k -s 640x360 ");
+    if (is_using_audio) fprintf(*bat, "-c:a aac -ac 2 -ab 32k -ar 44100 ");
+    fprintf(*bat, "-f flv -flush_packets 0 ");
+    fprintf(*bat, "rtmp://");
+    fprintf(*bat, "127.0.0.1"); // ip
+    fprintf(*bat, ":1935");
+    fclose(*bat); // close batch file
 }
 
-int main() {
-	FILE * fp, * bat;
-	string dvc_cmd, cmd_result;
+int main() 
+{
+    FILE* fp, *bat;
+    string device_command_input, device_command_output;
 
-	// TODO: GUI
+	/* GET DEVICE NAME */
+    device_command_input = "ffmpeg -list_devices true -f dshow -i dummy";
+    device_command_input.append(" 2>&1"); // redirect the stderr (>2) to where stdout is being redirected to (&1).
+    device_command_output = "";
+    fp = _popen(device_command_input.c_str(), "r"); // open cmd to read
+    if (!fp || feof(fp)) // file pointer error || EOF error
+	{ 
+        perror("errno: ");
+        return -1;
+    }	
 
-/* GET DEVICE NAME */
-	dvc_cmd = "ffmpeg -list_devices true -f dshow -i dummy";
-	dvc_cmd.append(" 2>&1"); // redirect the stderr (>2) to where stdout is being redirected to (&1).
-	cmd_result = "";
-	fp = _popen(dvc_cmd.c_str(), "r"); // open cmd to read
-	if (!fp || feof(fp)) { // file pointer error || EOF error
-		perror("errno: ");
-		return -1;
-	}	
-	// TODO: I/O error
+	ReadFromCommandLine(&fp, &device_command_output);
 
-	read_from_cmd(&fp, &cmd_result);
+	/* WRITE TO BATCH FILE */
+    // TODO: check if the device is available
+    bat = fopen("batch.bat", "w+");
+    if (!bat) 
+	{
+        perror("errno: ");
+        return -1;
+    }
+	else 
+    {
+		WriteBatchFile(&bat);
+		system("batch.bat");
+    }
 
-/* WRITE TO BATCH FILE */
-	// TODO: check if device is available
-	bat = fopen("batch.bat", "w+");
-	if (!bat) {
-		perror("errno: ");
-		return -1;
-	}
-	else write_to_bat(&bat);
-
-	system("batch.bat");
-//	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
